521ae71974027a8a2733a9ec54871e9ca0e9ce89
kalaikalaivanan <kalaikalaivanan81@gmail.com>
2024-07-11 14:54:41 +0530

    first commit

---
+++ week 2/practice/script.js
@@ -0,0 +1,31 @@
+//num = 23;
+//let A = (num%2) == 0 ? console.log("even") : console.log("odd");
+ 
+//sum of n numbers
+//function sumofnumbers(n){
+//    return n * (n+1) / 2;
+//}
+//console.log(sumofnumbers(2));
+//function recursiveSum(num) {
+// if (num === 0) {
+//      return 0;
+//  }
+// return (num % 10) + recursiveSum(Math.floor(num / 10));
+//}
+
+//console.log(recursiveSum(738))
+let a = "A Rope Ends It";
+let b = "DePrEsIaTiOn";
+let alower  = a.toLowerCase();
+let blower = b.toLowerCase();
+const myArra = Array.from(alower);
+const myArrb = Array.from(blower);
+let sorta = myArra.sort();
+let sortb = myArrb.sort();
+let ajoin = sorta.join('');
+let bjoin = sortb.join('');
+console.log(ajoin);
+console.log(bjoin);
+var equal = ajoin === bjoin ? console.log("equal") : console.log("not equal");
+
+
