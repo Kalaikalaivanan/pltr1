e96000f48bcdfd39f34c139c1977f31e5c5ce78c
Kalaikalaivanan <127074132+Kalaikalaivanan@users.noreply.github.com>
2024-07-13 18:31:59 +0530

    https://github.com/Kalaikalaivanan/pltr1.git

--- week 2/practice/script.js
+++ week 2/practice/script.js
@@ -1,7 +1,6 @@
 //num = 23;
 //let A = (num%2) == 0 ? console.log("even") : console.log("odd");
- 
-//sum of n numbers
+//sum of n number
 //function sumofnumbers(n){
 //    return n * (n+1) / 2;
 //}
@@ -12,20 +11,17 @@
 //  }
 // return (num % 10) + recursiveSum(Math.floor(num / 10));
 //}
-
 //console.log(recursiveSum(738))
-let a = "A Rope Ends It";
-let b = "DePrEsIaTiOn";
-let alower  = a.toLowerCase();
-let blower = b.toLowerCase();
-const myArra = Array.from(alower);
-const myArrb = Array.from(blower);
-let sorta = myArra.sort();
-let sortb = myArrb.sort();
-let ajoin = sorta.join('');
-let bjoin = sortb.join('');
-console.log(ajoin);
-console.log(bjoin);
-var equal = ajoin === bjoin ? console.log("equal") : console.log("not equal");
-
-
+//let a = "A Rope Ends It";
+//let b = "DePrEsIaTiOn";
+//let alower  = a.toLowerCase();
+//let blower = b.toLowerCase();
+//const myArra = Array.from(alower);
+//const myArrb = Array.from(blower);
+//let sorta = myArra.sort();
+//let sortb = myArrb.sort();
+//let ajoin = sorta.join('');
+//let bjoin = sortb.join('');
+//console.log(ajoin);
+//console.log(bjoin);
+//var equal = ajoin === bjoin ? console.log("equal") : console.log("not equal");
